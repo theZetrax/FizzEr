8b3ba338b2013b13f2fe3dfff935b13c619c5c5c
Zetrax <zabjd22@gmail.com>
2019-07-31 12:33:20 +0300

    added rounds for fizzbuzz

--- FizzEr/Program.cs
+++ FizzEr/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzEr
 {
@@ -10,6 +6,21 @@ namespace FizzEr
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter Rounds: ");
+            try
+            {
+                short rounds = Convert.ToInt16(Console.ReadLine());
+            } catch(FormatException fex)
+            {
+                Console.WriteLine("Error Ocurred: Wrong Format");
+                return;
+            } catch (OverflowException oex)
+            {
+                Console.WriteLine("Error Ocurred: OverFlow Ocurred");
+            }
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
