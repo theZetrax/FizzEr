85edee41ec3046e1afd3cbc9074ce9ace3dac9bb
Zetrax <zabjd22@gmail.com>
2019-07-31 12:49:50 +0300

    added fizzbuzz logic

--- FizzEr/Program.cs
+++ FizzEr/Program.cs
@@ -6,17 +6,28 @@ namespace FizzEr
     {
         static void Main(string[] args)
         {
+            int rounds = 0;
             Console.Write("Enter Rounds: ");
+
             try
             {
-                short rounds = Convert.ToInt16(Console.ReadLine());
+                rounds = Convert.ToInt16(Console.ReadLine());
             } catch(FormatException fex)
             {
                 Console.WriteLine("Error Ocurred: Wrong Format");
-                return;
+                // TODO: Add more infor error option
             } catch (OverflowException oex)
             {
-                Console.WriteLine("Error Ocurred: OverFlow Ocurred");
+                Console.WriteLine("Error Ocurred: OverFlow Ocurred.");
+                // TODO: Add more infor error option
+            }
+
+            for (var i = 1; i <= rounds; i++)
+            {
+                if (i % 3 == 0 && !(i % 5 == 0)) Console.WriteLine("Fizz");
+                if (i % 5 == 0 && !(i % 3 == 0)) Console.WriteLine("Buzz");
+                if ((i % 5 == 0) && (i % 3 == 0)) Console.WriteLine("Fizz Buzz");
+                else Console.WriteLine(i);
             }
 
             Console.Write("Press any key to exit...");
